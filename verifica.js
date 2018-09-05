eea416f3710b95f6e1c92f524ea432d7d07f5412
rogtejada <rogtejada@hotmail.com>
2018-09-05 02:43:52 -0300

    Mudanca na verificacao
    Mudando o try catch, para um laco de apenas IFs

--- verifica.js
+++ verifica.js
@@ -1,17 +1,20 @@
 function verifica1(){    
     var x = document.getElementById('numero1').value;
-    try {
-        if(x=='') throw 'Informe um Valor';
-        if(isNaN(x)) throw 'Voce precisa informar os numeros 1,2 ou 3';
+    if(x==''){
+        document.getElementById('msg1').innerHTML = 'Erro: Informe um Valor';
+    }else if(isNaN(x)){
+        document.getElementById('msg1').innerHTML = 'Erro: Voce precisa informar os numeros 1,2 ou 3';
+    }else if (x<1 || x>3){
         x = Number(x);
-        if(x<1 || x>3) throw 'Voce precisa informar os numero 1, 2 ou 3';        
+        document.getElementById('msg1').innerHTML = 'Erro: Voce precisa informar os numero 1, 2 ou 3';
+    }else if (x>=1 && x<=3){  
+        x =Number(x);      
         if(x==1) x='PEDRA';
         if(x==2) x='PAPEL';
         if(x==3) x='TESOURA'
         document.getElementById('msg1').innerHTML = 'Voce escolheu:' + x;
-    } catch (error) {
-        document.getElementById('msg1').innerHTML = 'Erro: ' + error;
     }
+        
 }
 function verifica2(){    
     var x = document.getElementById('numero2').value;
